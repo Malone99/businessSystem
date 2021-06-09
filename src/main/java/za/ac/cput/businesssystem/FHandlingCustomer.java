0ae5c0b220055d1751180b6c5168d591c8800861
Farai Chawora <malonechawora@gmail.com>
2021-06-09 18:32:58 +0200

    final commit

--- src/main/java/za/ac/cput/businesssystem/FHandlingCustomer.java
+++ src/main/java/za/ac/cput/businesssystem/FHandlingCustomer.java
@@ -26,191 +26,157 @@ import java.util.logging.Logger;
  * @author FARAI SKOOL
  */
 public class FHandlingCustomer {
-       FileWriter printWriter;
-    
- 
-    FileInputStream fis;
-    ObjectInputStream ois;
-    
-    public void openFile()
-    {
-        try
-        {
-              printWriter = new FileWriter(new File("customerOutFile.txt"));
-            
-            
-            
-        } catch (IOException ioe)
-        {
-            System.out.println("file failed to open because"+ioe.getMessage());
+
+    FileWriter fw;
+
+    FileInputStream fStream;
+    ObjectInputStream objectStream;
+
+    public void openFile() {
+        try {
+            fw = new FileWriter(new File("customerOutFile.txt"));
+
+        } catch (IOException ioe) {
+            System.out.println( ioe.getMessage());
             System.exit(1);
         }
     }
-    
-     private ArrayList<Customer> customersList()
-    {
+
+    private ArrayList<Customer> customersList() {
         ArrayList<Customer> customers = new ArrayList<>();
-        
-        try
-        {
-            fis = new FileInputStream("stakeholder.ser");
-            ois = new ObjectInputStream(fis);
-            
-            
-            while (true)
-            {
-                Object object = ois.readObject();
-                
-                if (object instanceof Customer)
-                {
+
+        try {
+            fStream = new FileInputStream("stakeholder.ser");
+            objectStream = new ObjectInputStream(fStream);
+
+            while (true) {
+                Object object = objectStream.readObject();
+
+                if (object instanceof Customer) {
                     customers.add((Customer) object);
                 }
             }
-            
-        } catch (EOFException eofe)
-        {
+
+        } catch (EOFException eofe) {
             System.out.println(eofe.getMessage());
-        } catch (IOException | ClassNotFoundException e)
-        {
+        } catch (IOException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
-           System.exit(1);
-            
-        } finally
-        {
-            
+            System.exit(1);
+
+        } finally {
+
             try {
-                fis.close();
-                ois.close();
+                fStream.close();
+                objectStream.close();
                 System.out.println("File Closed");
             } catch (IOException ex) {
                 System.out.println(ex.getMessage());
                 Logger.getLogger(FHandlingCustomer.class.getName()).log(Level.SEVERE, null, ex);
             }
-                
-           
+
         }
-       
-        if (!customers.isEmpty())
-        {
+
+        if (!customers.isEmpty()) {
             //sorting the arrray
-            Collections.sort(customers,(Customer c1, Customer c2) -> c1.getStHolderId().compareTo(c2.getStHolderId())
+            Collections.sort(customers, (Customer c1, Customer c2) -> c1.getStHolderId().compareTo(c2.getStHolderId())
             );
         }
-        
+
         return customers;
     }
-         private int canRentCar()
-    {
-        int canRent = 0;
-        
-        for (int i = 0; i < customersList().size(); i++)
-        {
-            if (customersList().get(i).getCanRent())
-            {
-                canRent += 1;
+
+    private int canRentCar() {
+        int rentCar = 0;
+
+        for (int i = 0; i < customersList().size(); i++) {
+            if (customersList().get(i).getCanRent()) {
+                rentCar += 1;
             }
         }
-        
-        return canRent;
+
+        return rentCar;
     }
-          
-    private int canNotRentCar()
-    {
-        int canNotRent = 0;
-        
-        for (int i = 0; i < customersList().size(); i++)
-        {
-            
-            if (!customersList().get(i).getCanRent())
-            {
-                canNotRent += 1;
+
+    private int canNotRentCar() {
+        int cantRent = 0;
+
+        for (int i = 0; i < customersList().size(); i++) {
+
+            if (!customersList().get(i).getCanRent()) {
+                cantRent += 1;
             }
         }
-        return canNotRent;
+        return cantRent;
     }
-        private void writeCustomerOutfile()
-    {
-        
-        
-        try
-        {   
-            String print="------------------------ CUSTOMERS -------------------------------------------------";
-            String prin="ID\t Name\t Surname\t   Date Of Birth\t  Age\t ";
-             String line="--------------------------------------------------------------------------------";
-             printWriter.write(print);
-             printWriter.write("\n");
-             printWriter.write(prin);
-             printWriter.write("\n");
-             printWriter.write(line);
-             printWriter.write("\n");
-            for (int i = 0; i < customersList().size(); i++)
-            {   
-                printWriter.write(
-                       
-                        customersList().get(i).getStHolderId()+"\t"+
-                        customersList().get(i).getFirstName()+"\t"+
-                        customersList().get(i).getSurName()+"\t"+
-                        reFormatDate(customersList().get(i).getDateOfBirth())+"\t"+
-                        calculateAge(customersList().get(i).getDateOfBirth())+"\n"
+
+    private void customerOutfile() {
+
+        try {
+            String print = "------------------------ CUSTOMERS -------------------------------------------------";
+            String prin = "ID\t Name\t Surname\t   Date Of Birth\t  Age\t ";
+            String line = "--------------------------------------------------------------------------------";
+            fw.write(print);
+            fw.write("\n");
+            fw.write(prin);
+            fw.write("\n");
+            fw.write(line);
+            fw.write("\n");
+            for (int i = 0; i < customersList().size(); i++) {
+                fw.write(
+                        customersList().get(i).getStHolderId() + "\t"
+                        + customersList().get(i).getFirstName() + "\t"
+                        + customersList().get(i).getSurName() + "\t"
+                        + reFormatDate(customersList().get(i).getDateOfBirth()) + "\t"
+                        + calculateAge(customersList().get(i).getDateOfBirth()) + "\n"
                 );
             }
-            
-            printWriter.write(
-                    "\nNumber of customers who can rent:  "+ 
-                    canRentCar());
-            
-            printWriter.write(
-                    "\nNumber of customers who cannot rent:  "+ 
-                    canNotRentCar());
-            
-        } catch (Exception e)
-        {
+
+            fw.write(
+                    "\nNumber of customers who can rent:  "
+                    + canRentCar());
+
+            fw.write(
+                    "\nNumber of customers who cannot rent:  "
+                    + canNotRentCar());
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-        
-        private String reFormatDate(String dob)
-    {
-       
+
+    private String reFormatDate(String dob) {
+
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MMM yyyy", Locale.ENGLISH);
-        LocalDate changeDob = LocalDate.parse(dob); 
+        LocalDate changeDob = LocalDate.parse(dob);
         return changeDob.format(dtf);
     }
-    
-    private int calculateAge(String dob)
-    {
-        LocalDate changeDob = LocalDate.parse(dob); 
-        int dobYear  = changeDob.getYear();
-        
+
+    private int calculateAge(String dob) {
+        LocalDate changeDob = LocalDate.parse(dob);
+        int dobYear = changeDob.getYear();
+
         ZonedDateTime currentDate = ZonedDateTime.now();
         int currentYear = currentDate.getYear();
-        
-       
+
         return currentYear - dobYear;
     }
-    
-         public void closeFile()
-    {
-        try
-        {
-            
-            printWriter.close();
-           
 
-        } catch (IOException ex)
-        {
+    public void closeFile() {
+        try {
+
+            fw.close();
+
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
-          public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         FHandlingCustomer FH = new FHandlingCustomer();
-        
+
         FH.openFile();
-        FH.writeCustomerOutfile();
+        FH.customerOutfile();
         FH.closeFile();
-       // FH.openFile("supplierOutFile.txt");
-        //FH.writeSupplierOutFile();
-       // FH.closeFile("supplierOutFile.txt");
+
     }
 }
--- src/main/java/za/ac/cput/businesssystem/FHanlingSuppliers.java
+++ src/main/java/za/ac/cput/businesssystem/FHanlingSuppliers.java
@@ -20,123 +20,96 @@ import java.util.Collections;
  * @author FARAI SKOOL
  */
 public class FHanlingSuppliers {
-           FileWriter printWriter;
 
-    FileInputStream fis;
-    ObjectInputStream ois;
-    
-    public void openFile()
-    {
-        try
-        {
-              printWriter = new FileWriter(new File("suplierOutFile.txt"));
-            
-            
-            
-        } catch (IOException ioe)
-        {
-            System.out.println("file failed to open because"+ioe.getMessage());
+    FileWriter fw;
+
+    FileInputStream FStream;
+    ObjectInputStream objectStream;
+
+    public void openFile() {
+        try {
+            fw = new FileWriter(new File("supplierOutFile.txt"));
+
+        } catch (IOException ioe) {
+            System.out.println("file failed to open because" + ioe.getMessage());
             System.exit(1);
         }
     }
-     private ArrayList<Supplier> suppliersList()
-    {
+
+    private ArrayList<Supplier> suppliersList() {
         ArrayList<Supplier> suppliers = new ArrayList<>();
-        
-        try
-        {
-            fis = new FileInputStream("stakeholder.ser");
-            ois = new ObjectInputStream(fis);
-            
-           
-            while (true)
-            {
-                Object object = ois.readObject();
-                
-                if (object instanceof Supplier)
-                {
+
+        try {
+            FStream = new FileInputStream("stakeholder.ser");
+            objectStream = new ObjectInputStream(FStream);
+
+            while (true) {
+                Object object = objectStream.readObject();
+
+                if (object instanceof Supplier) {
                     suppliers.add((Supplier) object);
                 }
             }
-            
-        } catch (EOFException eofe)
-        {
-            
-        } catch (IOException | ClassNotFoundException e)
-        {
+
+        } catch (EOFException eofe) {
+
+        } catch (IOException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
-            
-        } finally
-        {
-            try
-            {
-                fis.close();
-                ois.close();
-                
-            } catch (IOException e)
-            {
-                System.out.println( e.getMessage());
+
+        } finally {
+            try {
+                FStream.close();
+                objectStream.close();
+
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
             }
         }
-        
-        
-        if (!suppliers.isEmpty())
-        {
-            
+
+        if (!suppliers.isEmpty()) {
+
             Collections.sort(suppliers, (Supplier s1, Supplier s2) -> s1.getName().compareTo(s2.getName())
             );
         }
-        
+
         return suppliers;
     }
-     
-       private void writeSupplierOutFile()
-    {
-        
-        try
-        {
-            printWriter.write("----------------------------------------------- Supliers--------------------------------------------------------------------\n");
-            printWriter.write( "ID\t Name\t\t Prod Type\t Description\n");
-            
-            printWriter.write("----------------------------------------------------------------------------------------------------------\n");
-            for (int i = 0; i < suppliersList().size(); i++)
-            {
-                printWriter.write(
-                         
-                        suppliersList().get(i).getStHolderId()+"\t"+
-                        suppliersList().get(i).getName()+"\t"+
-                        suppliersList().get(i).getProductType()+"\t\t"+
-                        suppliersList().get(i).getProductDescription()+"\n"
+
+    private void supplierOutFile() {
+
+        try {
+            fw.write("----------------------------------------------- Supliers--------------------------------------------------------------------\n");
+            fw.write("ID\t Name\t\t Prod Type\t Description\n");
+
+            fw.write("----------------------------------------------------------------------------------------------------------\n");
+            for (int i = 0; i < suppliersList().size(); i++) {
+                fw.write(suppliersList().get(i).getStHolderId() + "\t" + suppliersList().get(i).getName() + "\t"
+                        + suppliersList().get(i).getProductType() + "\t\t"
+                        + suppliersList().get(i).getProductDescription() + "\n"
                 );
             }
-            
-        } catch (Exception e)
-        {
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-             public void closeFile()
-    {
-        try
-        {
-            
-            printWriter.close();
-           
 
-        } catch (IOException ex)
-        {
+    public void closeFile() {
+        try {
+
+            fw.close();
+
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
-               public static void main(String[] args)
-    {
-        FHanlingSuppliers FHS = new FHanlingSuppliers ();
-        
-        //FH.openFile();
-        //FH.writeCustomerOutfile();
-        //FH.closeFile();
+
+    public static void main(String[] args) {
+        FHanlingSuppliers FHS = new FHanlingSuppliers();
+
+       
         FHS.openFile();
-        FHS.writeSupplierOutFile();
+        FHS.supplierOutFile();
         FHS.closeFile();
     }
 }
